80ad22d2213aaf4be6e265dfff02a4a277130b0a
z7 <sandu@lungu.info>
2013-04-21 02:50:41 +0300

    Added front-end project.

---
+++ app.js
@@ -0,0 +1,55 @@
+requirejs.config({
+    paths: {
+        jquery: '//cdnjs.cloudflare.com/ajax/libs/jquery/1.9.1/jquery',
+        bootstrap: '//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/2.3.0/js/bootstrap',
+        underscore: '//cdnjs.cloudflare.com/ajax/libs/underscore.js/1.4.4/underscore'
+    },
+    shim: {
+        bootstrap: { deps: ['jquery'] }
+    }
+});
+
+/**
+ * Raspberry DJ client
+ */
+define(['jquery', 'bootstrap', 'underscore'], function() {
+    $(function() {
+
+        function render(data) {
+            var queue = {};
+            _.each(data.queue, function(id, no) {
+                queue[id] = queue[id] || [];
+                queue[id].push(no + 1);
+            });
+
+            var $container = $('#playlist .dynamic-container').html(''),
+                tpl = _.template($('#playlist script').html());
+
+            _.each(data.songs, function(name, id) {
+                $container.append(tpl({
+                    id: id,
+                    name: name,
+                    queue: queue[id] ? queue[id].join(', ') : '',
+                    now_playing: data.now_playing == id
+                }));
+            });
+        }
+
+        function refresh() {
+            $.getJSON('http://10.10.0.45:8888/playlist', render);
+        }
+
+        refresh();
+        setTimeout(refresh, 10 * 1000);
+
+        $('#playlist').delegate('.queue', 'click', function() {
+            var $btn = $(this),
+                id = $btn.data('id');
+
+            $.getJSON('http://10.10.0.45:8888/queue/' + id, render);
+
+            return false;
+        });
+
+    });
+});
