64ea68490db28e32030cf2da7946a0f22fcdda27
z7 <sandu@lungu.info>
2013-04-20 22:34:34 +0300

    Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
-pi
+Jukebox
 ==
 
-Rasperry Pi hacking
+You'll need an application on FB for the authentication of Jukebox visitors.
+Be sure to activate "Website login with FaceBook" and set the URL (host and port) exacly as you'll configure it later.
+https://developers.facebook.com/apps/
+
+See setup.sh for a step-by-step SSH commands.
+
+Based on the guide:
+http://ziade.org/2012/07/01/a-raspberry-pi-juke-box-how-to/
+
+Powered by:
+https://github.com/lociii/jukebox
---
+++ setup.sh
@@ -0,0 +1,31 @@
+# Install dependencies
+sudo apt-get update
+sudo apt-get install tightvncserver
+sudo apt-get install alsa-utils ntpdate mpg123 python-virtualenv libshout3 libshout3-dev pkg-config python-dev
+
+# Replace "1360x700" with your favorite resolution
+vncserver -geometry 1360x700 :1
+
+# By default the sound is not activated, and once activated it plays via the HDMI port
+sudo modprobe snd_bcm2835
+sudo amixer cset numid=3 1
+
+# Install Jukebox
+sudo pip install virtualenv
+virtualenv --no-site-packages jukebox
+cd jukebox
+bin/pip install jukebox
+bin/jukebox jukebox_setup
+bin/jukebox syncdb
+
+# Run when ready to rock'n'roll
+bin/jukebox jukebox_mpg123 --start
+
+# Replace 10.10.0.45 with your hostname
+bin/jukebox runserver 10.10.0.45:10080
+
+# Replace "KEY" with your USB dongle name
+bin/jukebox jukebox_index --path=/media/KEY
+
+# There is a daemon to watch the music directory for changes and add them to the library
+bin/jukebox jukebox_watch --path=/media/KEY
